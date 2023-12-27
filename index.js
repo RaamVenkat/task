1c408214af6f7c613d143094072d8f6f44696c34
Santosh <santoshblackshield@gmail.com>
2023-12-27 11:28:31 +0530

    cleanup

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDb from './config/dbConnection.js';
-import userData from './public/data.js';
 import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
@@ -13,7 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/user', userRoutes);
-app.use('/data', userData);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Server Port: ${port}`));
